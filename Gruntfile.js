9aaa1f959b7f3a594a8299c4d1e637d942f704cf
Kevin Korte <kevinkorte@Kevin-Kortes-MacBook-Pro.local>
2014-08-20 22:05:41 -0700

    Bower and Grunt additions

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+  });
+
+  // Load the plugin.
+  
+
+  // Default task(s).
+  grunt.registerTask('default');
+
+};
