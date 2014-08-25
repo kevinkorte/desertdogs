c24d0138e5ed65a534d5837c4de6f5d1aa4dc529
Kevin Korte <kevinkorte@Kevin-Kortes-MacBook-Pro.local>
2014-08-25 00:09:44 -0700

    Nightly Blog Update

--- home.php
+++ home.php
@@ -7,42 +7,44 @@
 </div>
 
 <div class="l-ddu-container">
+	<div class="row">
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="l-ddu-blogwrapper">
+			<div class="m-ddu-blogbar">
+				<span class="t-text-blogbar">Latest Posts</span>
+				<span class="pull-right">
+					<?php wp_dropdown_categories(); ?>
+				</span>
+			</div><!--/.m-ddu-blogbar-->
+			<div class="m-ddu-blog-feed clearfix">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="m-ddu-single-post">
+						<div class="row">
+							<div class="l-featured-image">
+								<?php the_post_thumbnail( 'thumbnail' );?>
+							</div><!--/.feature-image-->
+							<div class="l-blog-body">
+								<div class="t-cat">
+									<?php the_category( '&bull;' );?>
+								</div><!--/.t-cat-->
+								<h1 class="t-blog-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
+								<div class="t-blog-excerpt"><?php the_excerpt();?></div>
+							</div><!--/.l-blog-body-->
+						</div><!--/.row-->
+						<div class="row">
+							<div class="m-blog-meta">
+								<span class="t-date"><?php the_date();?></span>
+								<span class="t-comment-count pull-right"><?php comments_number();?></span>
+							</div><!--/.m-blog-meta-->
+						</div><!--/.row-->
+					</div><!--/.m-ddu-single-post-->
+				<?php endwhile; else : ?>
+					<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php endif; ?>
+			</div><!--/.m-ddu-blog-feed-->
+		</div><!--/.l-ddu-blogwrapper-->
 
-	<?php if( has_post_thumbnail() ) { ?>
-
-		<div class="row">
-			<?php
-				//Should probably refactor this into a function
-				//http://css-tricks.com/snippets/wordpress/get-featured-image-url/
-				$thumb_id = get_post_thumbnail_id();
-				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-				$thumb_url = $thumb_url_array[0];
-			?>
-			<div class="m-post-wrapper has-image" style="background-image: url('<?php echo $thumb_url;?>');">
-				<span class="t-time"><?php the_date();?></span>
-				<h1><?php the_title();?></h1>
-				<?php the_content();?>
-			</div><!--/.m-post-wrapper-->
 	</div><!--/.row-->
-
-	<?php } ?>
-
-	<?php if( !has_post_thumbnail() ) { ?>
-
-		<div class="m-post-wrapper has-no-image">
-			<span class="t-time"><?php the_date();?></span>
-			<h1><?php the_title();?></h1>
-			<?php the_content();?>
-		</div><!--/.m-post-wrapper-->
-
-	<?php } ?>
-
-<?php endwhile; else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-
 </div><!--ddu-container-->
 
 
